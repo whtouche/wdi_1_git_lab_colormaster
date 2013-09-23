b407b91f533750197fb90139f272954c837ba3ba
Tom Dyer <tdyer1@gmail.com>
2013-09-23 16:05:49 -0400

    check for file contents

--- spec/has_file_spec.rb
+++ spec/has_file_spec.rb
@@ -3,16 +3,31 @@ require_relative 'spec_helper'
 
 
 describe "Test filesystem" do
-  %w{ green purple red unsorted}.each do |color|
-    it "should have directory named #{color}" do
-      File.directory?("git-quiz/#{color}").should be_true
+  describe "Test directories" do
+    %w{ green purple red unsorted}.each do |color|
+      it "should have directory named #{color}" do
+        File.directory?("color-master-quiz/#{color}").should be_true
+      end
     end
   end
 
-  %w{ green/tree.txt purple/plum.txt red/apple.txt red/fire_truck.txt unsorted/ruby.txt unsorted/school_bus.txt}.each do |file|
-    it "should have file #{file}" do
-      File.exists?("git-quiz/#{file}").should be_true
-      File.ftype("git-quiz/#{file}").should == "file"
+  describe "Test Files" do
+    %w{ green/tree.txt purple/plum.txt red/apple.txt red/firetruck.txt unsorted/ruby.txt unsorted/school_bus.txt}.each do |file|
+      it "should have the file #{file}" do
+        File.exists?("color-master-quiz/#{file}").should be_true
+        File.ftype("color-master-quiz/#{file}").should == "file"
+      end
+
+      it "should have the correct description for #{file}" do
+        File.open("color-master-quiz/#{file}") do |f|
+          contents = f.readline
+          # contents.should =~ file.split('.').first
+          good_value = file.split('.').first.split('/').last
+          #puts "xx = #{good_value}"
+          # contents.should =~ /good_value/
+          #puts "contents are: \n #{contents}"
+        end
+      end
     end
   end
 end
