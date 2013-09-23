456220b59d9a2fd2e3e7898db1a937adc5d3e6ac
Tom Dyer <tdyer1@gmail.com>
2013-09-23 19:53:18 -0400

    Rest of the tests

--- spec/challenge3_spec.rb
+++ spec/challenge3_spec.rb
@@ -1,7 +1,20 @@
 require_relative 'spec_helper'
 
-describe "Challenge 2" do
+describe "Challenge 3" do
   it "should not have a unsorted directory" do
       File.directory?("color-master-quiz/unsorted").should be_false
+  end
+
+  # ruby.txt, school_bus.txt
+  it 'should have the file ruby.txt in the red directory' do
+    File.ftype("color-master-quiz/red/ruby.txt").should == "file"
+  end
+
+  it 'should have the file school_bus.txt in the yellow directory' do
+    File.ftype("color-master-quiz/yellow/school_bus.txt").should == "file"
+  end
+
+  it 'should have the file .hiding_turtle.txt in the green directory' do
+    File.ftype("color-master-quiz/green/.hiding_turtle.txt").should == "file"
   end
 end
